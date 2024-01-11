d1e98b3bf0ca523ee73501d461a610c94fe6de6f
Akhil-Sharma-26 <2004akhil26sharma@gmail.com>
2024-01-11 23:41:34 +0530

    added user and video modals with methods for bcrypt and jwt setup

---
+++ src/models/user.model.js
@@ -0,0 +1,138 @@
+import mongoose from "mongoose";
+import DB_NAME from '../constants.js';
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim: true, // trim is used to remove the extra spaces from the username
+        index: true, // index is used to make the search faster. optimizes search
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim: true,
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim: true,
+        index: true,
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"],
+    },
+    avatar:{
+        type:String, // url of the image using cloudinary
+        required:true
+    },
+    // bio:{
+    //     type:String,
+    //     required:false
+    // },
+    coverImage:{
+        type:String,
+        required:false
+    },
+    // followers:{
+    //     type:Array,
+    //     required:false
+    // },
+    // following:{
+    //     type:Array,
+    //     required:false
+    // },
+    // posts:{
+    //     type:Array,
+    //     required:false
+    // },
+    // likedPosts:{
+    //     type:Array,
+    //     required:false
+    // },
+    // savedPosts:{
+    //     type:Array,
+    //     required:false
+    // },
+    videosPublished:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:"video",
+            required:false
+        }
+    ],
+    watchedHistory:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:"video",
+            required:false
+        }
+    ],
+    likedVideos:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:"video",
+            required:false
+        }
+    ],
+    refreshToken:{
+        type:String,
+        required:false
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+        required:true
+    },
+    isBlocked:{
+        type:Boolean,
+        default:false,
+        required:true
+    },
+    
+},{timestamps:true})
+
+userSchema.pre("save",async function(next){
+    const user = this;
+    if(user.isModified("password")){
+        user.password = await bcrypt.hash(user.password,12);
+    }
+    next();
+})
+userSchema.methods.isPasswordCorrect = async function(password){
+    const user = this;
+    return await bcrypt.compare(password,user.password);
+}
+userSchema.methods.generateAccessToken = async function(){
+    const user = this;
+    const token = await jwt.sign({
+        id:user._id,
+        email:user.email,
+        username:user.username,
+        fullName:user.fullName,
+    },
+    process.env.ACCESS_TOKEN_SE,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    });
+    return token;
+}
+userSchema.methods.generateRefreshToken = async function(){
+    const user = this;
+    const token = await jwt.sign(
+        {id:user._id},
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn:process.env.REFRESH_TOKEN_EXPIRY});
+    return token;
+}
+const User = mongoose.model("User",userSchema);
+
+export default User;
---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema({
+    videoFile:{
+        type:String, // url of the video using cloudinary
+        required:true
+    },
+    thumbnail:{
+        type:String, // url of the image using cloudinary
+        required:true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref:"user",
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String, 
+        required:false
+    },
+    views:{
+        type:Number,
+        required:false
+    },
+    duration:{
+        type:Number, // data of vid using cloudinary
+        required:true
+    },
+    isPublished:{
+        type:Boolean,
+        default: true,
+        required:true
+    },
+},{timestamps:true})
+
+videoSchema.plugin(mongooseAggregatePaginate); // mongooseAggregatePaginate is used to paginate the data ie. to show the data in pages
+
+const Video = mongoose.model("Video",videoSchema);
+export default Video;
